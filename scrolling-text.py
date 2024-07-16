31b0edbf384b7d345d8c7bbf9b8c53505befc8f8
nightmono <163682003+nightmono@users.noreply.github.com>
2024-07-16 22:10:14 +0100

    Text end-scroll now working

--- scrolling-text.py
+++ scrolling-text.py
@@ -8,9 +8,16 @@ def scrolling_text(text: str):
     frame = 0
     max_frame = len(text)
     while 1:
-        output = f"{text[0:frame]:>{max_frame}}"
+        if frame <= max_frame:
+            output = f"{text[0:frame]:>{max_frame}}"
+        else:
+            output = f"{text[frame-max_frame:max_frame]:<{max_frame}}"
+
         print(f"{ERASE_LINE}{output}", end="")
+        
         frame += 1
+        if frame > max_frame * 2:
+            frame = 0
         time.sleep(0.1)
 
 if __name__ == "__main__":
