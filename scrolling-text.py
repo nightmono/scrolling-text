78a04e4ee1d87a1ca8c5fe5030f5cb474a2656c9
nightmono <163682003+nightmono@users.noreply.github.com>
2024-07-18 16:01:02 +0100

    Scrolling text works but only when the text is greater than the width

--- scrolling-text.py
+++ scrolling-text.py
@@ -4,7 +4,7 @@ import time
 
 ERASE_LINE = "\033[2K\r"
 
-def scrolling_text(text: str):
+def scrolling_text(text: str, width: int = 16):
     # The text will have 3 states that will need to be handled differently.
     
     # Left-end:
@@ -19,9 +19,26 @@ def scrolling_text(text: str):
     # The end (right) portion scrolling off
     # |an example  |
 
-    raise NotImplementedError()
+    start_i = 0
+    end_i = 0
+
+    while 1:
+        if end_i < width:
+            output_line = f"|{text[start_i:end_i]:>{width}}|"
+            end_i += 1
+        elif end_i <= len(text):
+            output_line = f"|{text[start_i:end_i]}|"
+            start_i += 1
+            end_i += 1
+        elif start_i <= len(text):
+            output_line = f"|{text[start_i:end_i]:<{width}}|"
+            start_i += 1
+        else:
+            start_i = 0
+            end_i = 1
+
+        print(f"{ERASE_LINE}{output_line}", end="")
+        time.sleep(0.1)
 
 if __name__ == "__main__":
-    text = input("> ")
-    print(f"\033[1A", end="")
-    scrolling_text(text)
+    scrolling_text("This is an example")
